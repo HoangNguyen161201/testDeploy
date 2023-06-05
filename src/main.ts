b78fcabfb3640b41718a23b94118996d4d638316
hoang nguyen <hoangdev161201@gmail.com>
2023-06-05 15:19:58 +0700

    upgrade

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import proxy from 'http-proxy-middleware'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
