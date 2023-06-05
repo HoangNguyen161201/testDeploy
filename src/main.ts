0ffe852898979a3eab187c684b67dac8a2417969
cuanh1612 <huynqdev1612@gmail.com>
2023-06-05 23:19:08 +0700

    fix port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  await app.listen(8080, '0.0.0.0');
   console.log('60');
 }
 bootstrap();
