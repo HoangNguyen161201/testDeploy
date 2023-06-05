64ec9c15f16df0ced80b91f6e4cc923e6a78028a
hoang nguyen <hoangdev161201@gmail.com>
2023-06-05 15:42:25 +0700

    upgrade

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const proxy = createProxyMiddleware('/api', {
+    target: 'http://45.77.255.150', // Thay thế bằng địa chỉ proxy của bạn
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api': '', // Tùy chỉnh đường dẫn tới proxy nếu cần thiết
+    },
+  });
+
+  app.use('/api', proxy);
+
   await app.listen(8080);
 }
 bootstrap();
